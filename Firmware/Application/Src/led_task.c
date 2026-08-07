5da2bd861567325167d3f9a98c9c61c0999848da
Eric <ericlin2023>
2026-08-07 13:06:50 -0400

    feat: add state-dependent LED blinking period

--- Firmware/Application/Src/led_task.c
+++ Firmware/Application/Src/led_task.c
@@ -11,21 +11,56 @@
 
 #include "gpio.h"
 
+#include "system_state_manager.h"
+
 #define LED_TASK_STACK_DEPTH_WORDS  128U
 #define LED_TASK_PRIORITY           (tskIDLE_PRIORITY + 1U)
-#define LED_TASK_PERIOD_MS          1000U
+#define LED_TASK_PERIOD_MS_BOOT		5000U
+#define LED_TASK_PERIOD_MS_INIT		500U
+#define LED_TASK_PERIOD_MS_STOPPED	1000U
+#define LED_TASK_PERIOD_MS_RUNNING	0U
+#define LED_TASK_PERIOD_MS_FAULT	50U
+
 
 static void LED_Task(void *argument)
 {
   TickType_t lastWakeTime;
+  TickType_t periodTicks;
+  SystemState_t currentState;
 
   (void)argument;
   lastWakeTime = xTaskGetTickCount();
 
   for (;;)
   {
+	currentState = SystemStateManager_GetState();
+	/**
+	 * LED has different blinking period according to the system states.
+	 */
+	switch (currentState)
+	{
+	case SYSTEM_STATE_BOOT:
+		periodTicks = pdMS_TO_TICKS(LED_TASK_PERIOD_MS_BOOT);
+		break;
+
+	case SYSTEM_STATE_INIT:
+		periodTicks = pdMS_TO_TICKS(LED_TASK_PERIOD_MS_INIT);
+		break;
+
+	case SYSTEM_STATE_STOPPED:
+		periodTicks = pdMS_TO_TICKS(LED_TASK_PERIOD_MS_STOPPED);
+		break;
+
+	case SYSTEM_STATE_RUNNING:
+		periodTicks = pdMS_TO_TICKS(LED_TASK_PERIOD_MS_RUNNING);
+		break;
+
+	case SYSTEM_STATE_FAULT:
+		periodTicks = pdMS_TO_TICKS(LED_TASK_PERIOD_MS_FAULT);
+	}
+
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-    vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(LED_TASK_PERIOD_MS));
+    vTaskDelayUntil(&lastWakeTime, periodTicks);
   }
 }
 
